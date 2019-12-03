5b078ca4eb7ac1990ebae15787af9528f72391b0
SamIsLost <sara.r433@stu.nebo.edu>
2019-12-03 10:26:28 -0700

    hyuh? yea, player movement bois. its in. its new. its fresh. its FUNKY.

---
+++ never-enough-chickens/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    public float rotateSpeed = 260.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //move player forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
+        //turn player
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * horizontalInput);
+    }
+}
